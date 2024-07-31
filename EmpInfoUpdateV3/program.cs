bfd220eba89906ac2af79997eda7d78281e05827
carrosborn <carr.osborn@vallartasupermarkets.com>
2024-07-31 09:22:59 -0700

    setup

--- EmpInfoUpdateV3/program.cs
+++ EmpInfoUpdateV3/program.cs
@@ -1,3 +1,20 @@
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+//using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+//using System.Drawing;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+using Microsoft.VisualBasic.FileIO;
+using System.Text.RegularExpressions;
+using WinSCP;
+using System.Runtime.ExceptionServices;
+
 namespace EmpInfoUpdateV3
 {
     public partial class MainClass
